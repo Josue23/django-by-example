d1c59de7fef967f194063fdff37e13bdcfaa8470
josue23 <josuerodrigues@gmail.com>
2018-02-12 12:33:55 -0200

    fix

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -3,19 +3,20 @@
 from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
-    # name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'autofocus': 'autofocus'}))
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
         widget=forms.Textarea)
     
 
-    # def send_mail(self):
-        # cd = self.cleaned_data
-        # post_url = request.build_absolute_uri(post.get_absolute_url())
-        # subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
-        # message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-        # send_mail(subject, message, 'admin@myblog.com',[cd['to']])
+class CommentForm(forms.ModelForm):
+    name =  forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'autofocus': 'autofocus'}))
+    email =  forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    body = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
