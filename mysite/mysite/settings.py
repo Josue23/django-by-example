1d822c858d98df7f5ca543d9cbb25a7f934fee78
josue23 <josuerodrigues@gmail.com>
2018-02-13 00:10:11 -0200

    Deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blog-do-josue.herokuapp.com']
 
 
 # Application definition
@@ -178,3 +178,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+# https://blog-do-josue.herokuapp.com/ | https://git.heroku.com/blog-do-josue.git
